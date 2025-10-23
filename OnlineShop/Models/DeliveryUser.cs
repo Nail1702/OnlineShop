e9a0bedbc9157569b9b8a75cbca42d4678db94d5
Nail1702 <ahunovnail80@gmail.com>
2025-10-23 19:54:37 +0300

    исправил баги

--- OnlineShop/Models/DeliveryUser.cs
+++ OnlineShop/Models/DeliveryUser.cs
@@ -1,9 +1,12 @@
-﻿namespace OnlineShop.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.Models
 {
     public class DeliveryUser
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+        public DateTime Date { get; set; }
         public string Address { get; set; }
         public string Phone { get; set; }
         public DateOnly Date { get; set; }
